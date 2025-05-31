ff59f126062d409d64126d14b87687daba5e8928
felixparey <mysteryparey@gmail.com>
2025-05-31 19:56:13 +0200

    Initial Commit

---
+++ FelixCV/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FelixCV
+//
+//  Created by Felix Parey on 31/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FelixCV/FelixCVApp.swift
@@ -0,0 +1,17 @@
+//
+//  FelixCVApp.swift
+//  FelixCV
+//
+//  Created by Felix Parey on 31/05/25.
+//
+
+import SwiftUI
+
+@main
+struct FelixCVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
