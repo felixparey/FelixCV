d8558d7f856e16f5432e5db7f8fe44f07bd633ef
felixparey <mysteryparey@gmail.com>
2025-06-11 18:32:03 +0200

    added more info on projects and education

--- FelixCV/Views/MainColumnView.swift
+++ FelixCV/Views/MainColumnView.swift
@@ -47,18 +47,27 @@ struct MainColumnView: View {
                 MainColumnSubheading("Lotus")
                 Text("iPad app developed for an NGO in Mumbai, India. It helps social workers find children's houses using MapKit and SwiftData, integrating with an external database for synchronization. The app was handed over by a previous team and adopted by my team and me")
                     .font(.system(size: 11))
+                    .padding(.bottom, 8)
+                
+                MainColumnSubheading("Others")
+                Text("Developed smaller projects showcasing knowledge of advanced technologies like RealityKit for visionOS, CoreML for AI integration, Firebase for backend services, StoreKit for in-app purchases, RESTful APIs for data fetching, and more. These projects demonstrate my ability to adapt and learn new technologies quickly.")
+                    .font(.system(size: 11))
             }
            
             Divider()
                 .padding(.vertical, 8)
             
             MainColumnCVSection("Education") {
-                MainColumnSubheading("Apple Developer Academy - PIER Student")
+                MainColumnSubheading("Apple Developer Academy - PIER Student (2024-2025)")
                 Text("Worked in an Agile Team using Scrum to work for an NGO in Mumbai, India. Development of an iPad app to help social workers of Schools near Slums to find children's houses using MapKit, SwiftData and an external Database.")
                     .font(.system(size: 11))
                     .padding(.bottom, 8)
-                MainColumnSubheading("Apple Developer Academy - Student")
+                MainColumnSubheading("Apple Developer Academy - Student (2023-2024)")
                 Text("Learned how to go from problem statement to an Application multiple times over. For that Swift and SwiftUI was used alongside other apple frameworks and eternal libraries")
+                    .font(.system(size: 11))
+                    .padding(.bottom, 8)
+                MainColumnSubheading("Innerstädtisches Gymnasium Rostock - Abitur (2021-2023)")
+                Text("Advanced Course in Informatics and English, representing my strong foundation in technology and communication. Elected Class representative for two years, demonstrating leadership and organizational skills.")
                     .font(.system(size: 11))
             }
             Spacer()
